318cbf1e095f1646bbf1e77e92dd945ed7121d05
Gurmeet Kaur <gurmeetkaur1272@gmail.com>
2026-02-22 18:37:19 +0530

    addANewFunc

---
+++ utils/index2.js
@@ -0,0 +1,11 @@
+const name = 'priyanka';
+const age = 32;
+const loc = 'Delhi';
+    if(age>=18){
+        console.log("she can vote!");
+    }
+    else {
+        console.log("Not"); 
+    }
+
+ 
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,10 @@
 function add(a,b){
     return a + b;
 }
+function sayName(){
+    console.log("Gurmeet Kaur");
+}
+function sayLoc(){
+    console.log('my location is ${Delhi}');
+}
+ 
